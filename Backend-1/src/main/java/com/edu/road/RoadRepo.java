ce1d469ca3205a9d229d96fb9957f63ec2d552f8
hahahahohohoho <tjstls9728@gmail.com>
2024-08-07 10:24:56 +0900

    이클립스 테스트

--- Backend-1/src/main/java/com/edu/road/RoadRepo.java
+++ Backend-1/src/main/java/com/edu/road/RoadRepo.java
@@ -1,7 +1,10 @@
 package com.edu.road;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface RoadRepo extends JpaRepository<Road, Long>{
-
+	List<Road> findByGrade(Integer grade);
+	List<Road> findByNameContaining(String name);
 }
